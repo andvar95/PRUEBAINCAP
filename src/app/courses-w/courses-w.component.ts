88d9a68a790dcd5220e2616543e22f05cd87a2e9
andvar95 <varleo.95@gmail.com>
2021-01-31 19:34:26 -0500

    Final_FInal

--- src/app/courses-w/courses-w.component.ts
+++ src/app/courses-w/courses-w.component.ts
@@ -107,7 +107,38 @@ console.log("entre")}
                   }}}
         });
   
-  });}
+  });
+
+}
+this.cross=0;
+this.hor1 = [];
+this.hor2 = [];
+if(this.cartservice.getMyCourses().length>0)//if cart is empty don't verify
+{
+Object.entries(this.cartservice.getMyCourses()).forEach(
+  ([keys,dict]:any) => {
+    Object.entries(dict.Schedule).forEach(([day,sche]:any)=>{
+      for(let courseSc in course.Schedule){
+        if((course.key != dict.key) && (course.Schedule[courseSc].split("-")[0] == sche.split("-")[0])){
+          this.hor1=course.Schedule[courseSc].split("-");
+          this.hor2=sche.split("-");              
+            for(let h1 of this.hor1){
+              for(let h2 of this.hor2){
+                if (h1 ==h2){
+                 this.cross++;
+                }}}
+            if(this.cross >=3){
+            
+              this.sleepMsj("Cruce de horarios con "+dict.title,course)
+                this.cross=0;
+                this.crossEna = false;
+                
+              }}}
+    });
+
+});
+
+}
   return this.crossEna;
 
   
